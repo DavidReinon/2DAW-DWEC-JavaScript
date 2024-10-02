60043214eb9485ca5b9130002ed25e24cf1a268d
DavidReinon <davidreinongarcia@gmail.com>
2024-10-02 13:05:30 +0200

    ejercicio 1 completado - evaluable 1

--- Evaluable01JavascriptAvanzado/Ejercicio 1 y 2/ejercicio1.js
+++ Evaluable01JavascriptAvanzado/Ejercicio 1 y 2/ejercicio1.js
@@ -1,33 +1,161 @@
-const pNames = document.getElementsByTagName("p");
-
 const fruits = [
-    { name: "Melon", url: "images/frutas/melon" },
-    { name: "Naranja", url: "images/frutas/orange" },
-    { name: "Melocoton", url: "images/frutas/peach" },
-    { name: "Fresa", url: "images/verduras/strawberry" },
-    { name: "Sandia", url: "images/verduras/watermelon" },
+    {
+        name: "Melon",
+        urls: [
+            "images/frutas/melon/1.jpg",
+            "images/frutas/melon/2.jpg",
+            "images/frutas/melon/3.jpg",
+            "images/frutas/melon/4.jpg",
+        ],
+    },
+    {
+        name: "Naranja",
+        urls: [
+            "images/frutas/orange/1.jpg",
+            "images/frutas/orange/2.jpg",
+            "images/frutas/orange/3.jpg",
+            "images/frutas/orange/4.jpg",
+        ],
+    },
+    {
+        name: "Melocoton",
+        urls: [
+            "images/frutas/peach/1.jpg",
+            "images/frutas/peach/2.jpg",
+            "images/frutas/peach/3.jpg",
+            "images/frutas/peach/4.jpg",
+        ],
+    },
+    {
+        name: "Fresa",
+        urls: [
+            "images/frutas/strawberry/1.jpg",
+            "images/frutas/strawberry/2.jpg",
+            "images/frutas/strawberry/3.jpg",
+            "images/frutas/strawberry/4.jpg",
+        ],
+    },
+    {
+        name: "Sandia",
+        urls: [
+            "images/frutas/watermelon/1.jpg",
+            "images/frutas/watermelon/2.jpg",
+            "images/frutas/watermelon/3.jpg",
+            "images/frutas/watermelon/4.jpg",
+        ],
+    },
 ];
 
 const vegetables = [
-    { name: "Cebolla dulce", url: "images/verduras/cebolla_dulce" },
-    { name: "Cebolla dulce", url: "images/verduras/cebolla_dulce" },
-    { name: "Cebolla dulce", url: "images/verduras/cebolla_dulce" },
-    { name: "Cebolla morada", url: "images/verduras/cebolla_morada" },
-    { name: "Tomate cherry", url: "images/verduras/tomate_cherry" },
-    { name: "Tomate raf", url: "images/verduras/tomate_raf" },
-    { name: "Tomate rosa", url: "images/verduras/tomate_rosa" },
+    {
+        name: "Cebolla dulce",
+        urls: [
+            "images/verduras/cebolla_dulce/1.jpg",
+            "images/verduras/cebolla_dulce/2.jpg",
+            "images/verduras/cebolla_dulce/3.jpg",
+            "images/verduras/cebolla_dulce/4.jpg",
+        ],
+    },
+    {
+        name: "Cebolla morada",
+        urls: [
+            "images/verduras/cebolla_morada/1.jpg",
+            "images/verduras/cebolla_morada/2.jpg",
+            "images/verduras/cebolla_morada/3.jpg",
+            "images/verduras/cebolla_morada/4.jpg",
+        ],
+    },
+    {
+        name: "Tomate cherry",
+        urls: [
+            "images/verduras/tomate_cherry/1.jpg",
+            "images/verduras/tomate_cherry/2.jpg",
+            "images/verduras/tomate_cherry/3.jpg",
+            "images/verduras/tomate_cherry/4.jpg",
+        ],
+    },
+    {
+        name: "Tomate raf",
+        urls: [
+            "images/verduras/tomate_raf/1.jpg",
+            "images/verduras/tomate_raf/2.jpg",
+            "images/verduras/tomate_raf/3.jpg",
+            "images/verduras/tomate_raf/4.jpg",
+        ],
+    },
+    {
+        name: "Tomate rosa",
+        urls: [
+            "images/verduras/tomate_rosa/1.jpg",
+            "images/verduras/tomate_rosa/2.jpg",
+            "images/verduras/tomate_rosa/3.jpg",
+            "images/verduras/tomate_rosa/4.jpg",
+        ],
+    },
 ];
 
-Array.from(pNames).forEach((element, index) => {
-    const defaultOpacity = "0.5";
+const pNames = document.getElementsByTagName("p");
+const currentItemsTypeTitle = document.getElementsByTagName("h1")[0];
+const imageH2Title = document.getElementsByTagName("h2")[0];
 
-    const changeOpacity = () => {
-        element.style.opacity =
-            element.style.opacity === defaultOpacity ? "1" : defaultOpacity;
+const imagesContainer = document.getElementsByTagName("img");
+const carusselImage = imagesContainer[0];
+const vignetteImage = imagesContainer[1];
+
+const buttons = document.getElementsByTagName("button");
+const previousButton = buttons[0];
+const nextButton = buttons[1];
+
+let imageIndex = 0;
+let currentItemIndex = 0;
+let currentItemsType = fruits;
+
+const updateVigneteImage = () => {
+    vignetteImage.src = currentItemsType[currentItemIndex].urls[imageIndex];
+};
+const updateCarusselImage = () => {
+    carusselImage.src = currentItemsType[currentItemIndex].urls[imageIndex];
+};
+const updateImageH2Title = () => {
+    imageH2Title.textContent = currentItemsType[currentItemIndex].name;
+};
+
+const buttonsFuncionality = () => {
+    const nextEventListener = () => {
+        imageIndex =
+            (imageIndex + 1) % currentItemsType[currentItemIndex].urls.length;
+        updateCarusselImage();
     };
-    
-    element.textContent = fruits[index].name;
-    element.style.opacity = defaultOpacity;
-    element.addEventListener("mouseover", changeOpacity);
-    element.addEventListener("mouseout", changeOpacity);
-});
+    const previousEventListener = () => {
+        imageIndex =
+            (imageIndex - 1 + currentItemsType[currentItemIndex].urls.length) %
+            currentItemsType[currentItemIndex].urls.length;
+        updateCarusselImage();
+    };
+
+    previousButton.addEventListener("click", previousEventListener);
+    nextButton.addEventListener("click", nextEventListener);
+};
+
+const mouseOverTitle = () => {
+    Array.from(pNames).forEach((element, index) => {
+        const defaultOpacity = "0.5";
+
+        const eventListener = () => {
+            element.style.opacity =
+                element.style.opacity === defaultOpacity ? "1" : defaultOpacity;
+            currentItemIndex = index;
+            imageIndex = 0;
+            updateImageH2Title();
+            updateVigneteImage();
+            updateCarusselImage();
+        };
+        element.textContent = currentItemsType[index].name;
+        element.style.opacity = defaultOpacity;
+        element.addEventListener("mouseover", eventListener);
+        element.addEventListener("mouseout", eventListener);
+    });
+};
+
+mouseOverTitle();
+buttonsFuncionality();
