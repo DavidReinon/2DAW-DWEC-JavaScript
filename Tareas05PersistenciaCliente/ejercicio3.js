4c7be34274a1cb48603774f672315afd7f30b7bb
DavidReinon <davidreinongarcia@gmail.com>
2024-10-28 09:21:20 +0100

    ejercicio 3 terminado

--- Tareas05PersistenciaCliente/ejercicio3.js
+++ Tareas05PersistenciaCliente/ejercicio3.js
@@ -1,63 +1,78 @@
 //Ejercicio 3
-const addMonument = (event) => {
-    event.preventDefault();
-    const monument = document.getElementsByName("monument")[0];
-    const country = document.getElementsByName("country")[0];
-    const photo = document.getElementsByName("photo")[0];
+document.addEventListener("DOMContentLoaded", () => {
+    const addMonument = (event) => {
+        event.preventDefault();
+        const monument = document.getElementsByName("monument")[0];
+        const country = document.getElementsByName("country")[0];
+        const photo = document.getElementsByName("photo")[0];
 
-    const data = {
-        monument: monument.value,
-        country: country.value,
-        photo: photo.value,
+        const data = {
+            monument: monument.value,
+            country: country.value,
+            photo: photo.value,
+        };
+
+        window.localStorage.setItem(monument.value, JSON.stringify(data));
     };
 
-    window.localStorage.setItem(monument.value, JSON.stringify(data));
-};
+    const displayMonument = (monumentName) => {
+        console.log(monumentName);
+        if (!monumentName) return;
 
-const displayMonument = (monumentName) => {
-    console.log(monumentName);
-    if (!monumentName) return;
+        const data = window.localStorage.getItem(monumentName);
 
-    const data = window.localStorage.getItem(monumentName);
+        const imageElement = document.getElementById("image");
+        const titleElement = document.getElementsByName("monument_title")[0];
+        const countryElement =
+            document.getElementsByName("monument_country")[0];
 
-    const imageElement = document.getElementById("image");
-    const titleElement = document.getElementsByName("monument_title")[0];
-    const countryElement = document.getElementsByName("monument_country")[0];
+        if (!data) {
+            titleElement.textContent = "No data for this Monument";
+            countryElement.textContent = "";
+            imageElement.src = "";
+            return;
+        }
 
-    if (!data) {
-        titleElement.textContent = "No data for this Monument";
-        countryElement.textContent = "";
-        imageElement.src = "";
-        return;
-    }
+        const parsedData = JSON.parse(data);
+        titleElement.textContent = "Monument: " + parsedData.monument;
+        countryElement.textContent = "Country: " + parsedData.country;
+        imageElement.src = parsedData.photo;
+        imageElement.alt = parsedData.monument;
+    };
 
-    const parsedData = JSON.parse(data);
-    titleElement.textContent = parsedData.monument;
-    countryElement.textContent = parsedData.country;
-    imageElement.src = parsedData.photo;
-    imageElement.alt = parsedData.monument;
-};
+    const deleteMonument = (monumentName) => {
+        if (!monumentName) return;
 
-const deleteMonument = (monumentName) => {
-    if (!monumentName) return;
+        window.localStorage.removeItem(monumentName);
+    };
 
-    window.localStorage.removeItem(monumentName);
-};
+    const deleteAllData = () => {
+        window.localStorage.clear();
+    };
 
-const deleteAllData = () => {
-    window.localStorage.clear();
-};
+    const init3 = () => {
+        const submitButton = document.querySelectorAll(
+            'button[type="submit"]'
+        )[2];
+        submitButton.addEventListener("click", addMonument);
 
+        const inputSearchMonument = document.getElementsByName("get_data")[2];
 
-const init3 = () => {
-    const submitButton = document.querySelectorAll('button[type="submit"]')[2];
-    submitButton.addEventListener("click", addMonument);
+        const buttonGetData = document.getElementsByName("button_get_data")[2];
+        buttonGetData.addEventListener("click", () =>
+            displayMonument(inputSearchMonument.value)
+        );
 
-    const inputSearchMonument = document.getElementsByName("get_data")[2];
-    const buttonGetData = document.getElementsByName("button_get_data")[2];
-    buttonGetData.addEventListener("click", () =>
-        displayMonument(inputSearchMonument.value)
-    );
-};
+        const buttoneDeleteItem =
+            document.getElementsByName("button_delete_item")[2];
+        buttoneDeleteItem.addEventListener("click", () =>
+            deleteMonument(inputSearchMonument.value)
+        );
 
-init3();
+        const buttonDeleteAll =
+            document.getElementsByName("button_delete_all")[2];
+        buttonDeleteAll.addEventListener("click", deleteAllData);
+    };
+
+    init3();
+});
